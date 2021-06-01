3e93beaeb140774b8851ed2880cda56502c3228a
AntonioCarbone <a.carbone@fpholding.it>
2021-06-01 16:43:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -340,5 +340,29 @@ module.exports = {
                 }
             });
         })
+    },
+    firmaDocumentiOTP: (json) => {
+        var options = {
+            method: 'POST',
+            url: u._BaseAddress(bu,'/api/dm7/firmaDocumento'),
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'Authorization': 'Bearer '+cache.get('_ta')
+            },
+            form: json
+        }
+        return new Promise((resolve, reject) => {
+            request(options, function (error, response) {
+                if (error) {
+                    reject(error);
+                } else {
+                    if(response.statusCode < 400) {
+                        resolve(response.body);
+                    } else {
+                        reject(response.body);
+                    }
+                }
+            });
+        })
     }
 };
